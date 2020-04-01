b72834b1ffd3f34ff9a3bba0e8087de9afd3b0ba
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-04-01 22:37:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,6 +54,7 @@ end
 
 def turn_count(board)
   # board.count{|token| token == "X" || token == "O"}
+  # 9 - board.count(" ")
   turns = 0
   board.each do |token|
     if token == "X" || token == "O"
