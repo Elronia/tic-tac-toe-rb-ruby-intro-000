1503008d0384547839cf31ac1e3e2240f455a752
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-04-01 23:36:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,8 +41,8 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip #receiving user's input
-#  index = input_to_index(input)
+  user_input = gets.strip #receiving user's input
+  index = input_to_index(user_input)
    if valid_move?(board, index)
       token = current_player(board)
       move(board, position, token)
